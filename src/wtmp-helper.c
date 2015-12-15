cad21b185af08b4aba79bf541549bb159a686e03
Alan Coopersmith <alan.coopersmith@oracle.com>
2015-12-14 18:57:27 -0800

    wtmp: fix wtmp file on solaris and netbsd
    This commit moves detection of the location
    of the wtmp file to configure and fixes that
    detection to work on Solaris and NetBSD.
    
    https://bugs.freedesktop.org/show_bug.cgi?id=90882

--- src/wtmp-helper.c
+++ src/wtmp-helper.c
@@ -56,12 +56,12 @@ user_previous_login_free (UserPreviousLogin *previous_login)
 static gboolean
 wtmp_helper_start (void)
 {
-#if defined(UTXDB_LOG)
+#if defined(HAVE_SETUTXDB)
                 if (setutxdb (UTXDB_LOG, NULL) != 0) {
                         return FALSE;
                 }
-#elif defined(WTMPX_FILENAME)
-                if (utmpxname (WTMPX_FILENAME) != 0) {
+#elif defined(PATH_WTMP)
+                if (utmpxname (PATH_WTMP) != 0) {
                         return FALSE;
                 }
 
@@ -212,13 +212,7 @@ wtmp_helper_entry_generator (GHashTable *users,
 const gchar *
 wtmp_helper_get_path_for_monitor (void)
 {
-#if defined(WTMPX_FILENAME)
-        return WTMPX_FILENAME;
-#elif defined(__FreeBSD__)
-        return "/var/log/utx.log";
-#else
-#error Do not know which filename to watch for wtmp changes
-#endif
+        return PATH_WTMP;
 }
 
 #else /* HAVE_UTMPX_H */
